7f48f4bba958a20fa3bd811d44ff674f26f63629
Arun Sundaram <26311830+asundr@users.noreply.github.com>
2026-02-11 11:35:53 +1100

    Added most recent chat overlay

---
+++ src/main/java/org/asundr/OverlayLastMessage.java
@@ -0,0 +1,89 @@
+package org.asundr;
+
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.MessageNode;
+import net.runelite.api.events.ChatMessage;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+import net.runelite.client.util.Text;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class OverlayLastMessage extends OverlayPanel
+{
+
+    private static OfferAtPriceConfig config;
+    private static HashMap<String, MessageNode> lastMessages = new HashMap<>();
+
+    private String lastMessage = null;
+
+    OverlayLastMessage(OfferAtPriceConfig config)
+    {
+        OverlayLastMessage.config = config;
+
+        setLayer(OverlayLayer.ALWAYS_ON_TOP);
+        setPreferredColor(Color.GREEN);
+        setBounds(new Rectangle(100,100));
+    }
+
+    @Subscribe
+    private void onEventTradeStateChanged(OfferManager.EventTradeStateChanged event)
+    {
+        lastMessage = null;
+    }
+
+    @Subscribe
+    private void onChatMessage(ChatMessage evt)
+    {
+        final String eventName = Text.removeTags(evt.getName());
+        if (eventName.isEmpty())
+        {
+            return;
+        }
+        lastMessages.put(eventName, evt.getMessageNode());
+        ChatMessageType type = evt.getType();
+        final String playerName = OfferManager.getOfferInfo().playerName;
+        if (!eventName.equals(playerName))
+        {
+            return;
+        }
+        lastMessage = Text.removeTags(evt.getMessage());
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics)
+    {
+        if (lastMessage == null || !config.showLastChat() || !OfferManager.isTrading())
+        {
+            return null;
+        }
+        panelComponent.getChildren().add(TitleComponent.builder()
+                .text(lastMessage)
+                .color(config.colorOfLastChatOverlay())
+                .build());
+        panelComponent.setPreferredSize(new Dimension(
+                graphics.getFontMetrics().stringWidth(lastMessage) + 10,
+                40));
+        final Rectangle rect = OfferManager.getTradeMenuLocation();
+        final int yOffset = -10;
+        setPreferredLocation(new java.awt.Point((int)rect.getX() + rect.width/2 - getBounds().width/2, yOffset + (int)rect.getY()));
+        return super.render(graphics);
+    }
+
+    public void onTradedPlayerNameChanged()
+    {
+        final String playerName = OfferManager.getOfferInfo().playerName;
+        if (playerName == null)
+        {
+            return;
+        }
+        final MessageNode lastMessageNode = lastMessages.get(playerName);
+        if (lastMessageNode != null)
+        {
+            lastMessage = Text.removeTags(lastMessageNode.getValue()).trim();
+        }
+    }
+}
